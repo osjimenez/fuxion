6d9fd54b2c241a7a8f782e9f86dd89f9f4dfe1ae
Oscar Jimenez <social@ittxa.com>
2016-01-19 15:08:17 +0100

    nuspec URLs updated

--- src/core/portable/Properties/AssemblyInfo.cs
+++ src/core/portable/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.419")]
-[assembly: AssemblyFileVersion("0.0.1.419")]
-[assembly: AssemblyInformationalVersion("0.0.1-alpha419")]
+[assembly: AssemblyVersion("0.0.1.420")]
+[assembly: AssemblyFileVersion("0.0.1.420")]
+[assembly: AssemblyInformationalVersion("0.0.1-alpha420")]
