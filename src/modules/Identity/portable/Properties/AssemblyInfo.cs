61359979d3342160e169529b6955029371074bea
Oscar Jimenez <social@ittxa.com>
2016-01-19 14:19:07 +0100

    Identity package

--- src/modules/Identity/portable/Properties/AssemblyInfo.cs
+++ src/modules/Identity/portable/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.416")]
-[assembly: AssemblyFileVersion("0.0.1.416")]
-[assembly: AssemblyInformationalVersion("0.0.1-alpha416")]
+[assembly: AssemblyVersion("0.0.1.1")]
+[assembly: AssemblyFileVersion("0.0.1.1")]
+[assembly: AssemblyInformationalVersion("0.0.1-alpha001")]
