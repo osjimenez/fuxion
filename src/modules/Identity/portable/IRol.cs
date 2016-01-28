0209857a4e9d4817c3697ea43d4b3ad2fa5e184d
Oscar Jimenez <social@ittxa.com>
2016-01-28 15:57:22 +0100

    Rol internal console null bug fixed

--- src/modules/Identity/portable/IRol.cs
+++ src/modules/Identity/portable/IRol.cs
@@ -22,7 +22,8 @@ namespace Fuxion.Identity
         public static bool IsValid(this IRol me) { return !string.IsNullOrWhiteSpace(me.Name); }
         internal static bool IsFunctionAssigned(this IRol me, IFunctionGraph functions, IFunction function, IDiscriminator[] discriminators, Action<string, bool> console, out IPermission deniedPermissionMatched)
         {
-            console($"Comprobando asignación de funciones\r\n   Rol: {me.Name}\r\n   Funcion: {function.Name}\r\n   Discriminadores:\r\n{discriminators.Aggregate("", (a, c) => $"{a}      {c.Name}\r\n")}", false);
+            Action<string, bool> con = (m, i) => { if (console != null) console(m, i); };
+            con($"Comprobando asignación de funciones\r\n   Rol: {me.Name}\r\n   Funcion: {function.Name}\r\n   Discriminadores:\r\n{discriminators.Aggregate("", (a, c) => $"{a}      {c.Name}\r\n")}", false);
             // Parameters validation
             if (!function.IsValid()) throw new InvalidStateException($"The '{nameof(function)}' parameter has an invalid state");
             if (discriminators == null || !discriminators.Any()) throw new ArgumentException($"The '{nameof(discriminators)}' pararameter cannot be null or empty", nameof(discriminators));
@@ -31,28 +32,28 @@ namespace Fuxion.Identity
             // First, must take only permissions that match with the function and discriminators
             if(me.Permissions == null || !me.Permissions.Any())
             {
-                console($"Resultado: NO VALIDO - El rol no tiene permisos definidos", true);
+                con($"Resultado: NO VALIDO - El rol no tiene permisos definidos", true);
                 deniedPermissionMatched = null;
                 return false;
             }
-            var matchs = me.Permissions.Where(p => p.Match(functions, function, discriminators, console)).ToList();
-            console($"   Match => {matchs.Count()}", true);
+            var matchs = me.Permissions.Where(p => p.Match(functions, function, discriminators, con)).ToList();
+            con($"   Match => {matchs.Count()}", true);
             // Now, let's go to check that does not match any denegation permission
             var den = matchs.FirstOrDefault(p => !p.Value);
             if (den != null)
             {
                 deniedPermissionMatched = den;
-                console($"Resultado: NO VALIDO - Denegado por un permiso", true);
+                con($"Resultado: NO VALIDO - Denegado por un permiso", true);
                 return false;
             }
             deniedPermissionMatched = null;
             // Now, let's go to check that match at least one grant permission
             if (!matchs.Any(p => p.Value))
             {
-                console($"Resultado: NO VALIDO - Denegado por no encontrar un permiso de concesión", true);
+                con($"Resultado: NO VALIDO - Denegado por no encontrar un permiso de concesión", true);
                 return false;
             }
-            console($"Resultado: VALIDO", true);
+            con($"Resultado: VALIDO", true);
             return true;
         }
         internal static void CheckFunctionAssigned(this IRol me, IFunctionGraph functions, IFunction function, IDiscriminator[] discriminators, Action<string,bool> console)
