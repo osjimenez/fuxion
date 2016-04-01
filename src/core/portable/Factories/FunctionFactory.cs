144179de39e3b2032c68341c57b03e14e045fe18
Oscar Jimenez <social@ittxa.com>
2016-04-01 12:38:14 +0200

    Instance & function factories

---
+++ src/core/portable/Factories/FunctionFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuxion.Factories
+{
+    public class FunctionFactory<T> : IFactory
+    {
+        public FunctionFactory(Func<T> createInstanceFunction) { this.createInstanceFunction = createInstanceFunction; }
+        public FunctionFactory(Func<IEnumerable<T>> createInstancesFunction) { this.createInstancesFunction = createInstancesFunction; }
+        bool instanceSetted = false;
+        Func<T> createInstanceFunction;
+        bool instancesSetted = false;
+        Func<IEnumerable<T>> createInstancesFunction;
+        public object Get(Type type)
+        {
+            if(!instanceSetted) throw new NotImplementedException();
+            return createInstanceFunction();
+        }
+
+        public IEnumerable<object> GetMany(Type type)
+        {
+            if (!instancesSetted) throw new NotImplementedException();
+            return (IEnumerable<object>)createInstancesFunction();
+        }
+    }
+}
---
+++ src/core/portable/Factories/InstanceFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuxion.Factories
+{
+    public class InstanceFactory<T> : IFactory
+    {
+        public InstanceFactory(T instance) { this.instance = instance; }
+        public InstanceFactory(IEnumerable<T> instances) { this.instances = instances; }
+        bool instanceSetted = false;
+        T instance;
+        bool instancesSetted = false;
+        IEnumerable<T> instances;
+        public object Get(Type type)
+        {
+            if (!instanceSetted) throw new NotImplementedException();
+            return instance;
+        }
+        public IEnumerable<object> GetMany(Type type)
+        {
+            if (!instancesSetted) throw new NotImplementedException();
+            return (IEnumerable<object>)instances;
+        }
+    }
+}
