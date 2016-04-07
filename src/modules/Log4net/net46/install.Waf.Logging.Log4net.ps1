f2a87d72de94715e53bf6be4b0fe810b12615c4b
Oscar Jimenez <social@ittxa.com>
2016-04-07 15:05:30 +0200

    BUG: log4net project target now remove existing elements

--- src/modules/Log4net/net46/install.Waf.Logging.Log4net.ps1
+++ src/modules/Log4net/net46/install.Waf.Logging.Log4net.ps1
@@ -1,14 +1,21 @@
 param($installPath, $toolsPath, $package, $project)
-$project.ProjectItems.Item("log4net.config").Properties.Item("CopyToOutputDirectory").Value = 2
 
-$project = Get-Project
+Write-Host "Configuring project for Fuxion.log4net behavior"
+
+$project.ProjectItems.Item("log4net.config").Properties.Item("CopyToOutputDirectory").Value = 2
 $buildProject = Get-MSBuildProject $project.ProjectName
-$target = $buildProject.Xml.AddTarget("AfterBuild")
-#$target.AfterTargets = "AfterBuild"
+ForEach ($child in $buildProject.Xml.Targets){
+	If ($child.Name -eq "Fuxion-Log4net-Rename-Config-File")
+	{
+		$buildProject.Xml.RemoveChild($child)
+	}
+}
+$target = $buildProject.Xml.AddTarget("Fuxion-Log4net-Rename-Config-File")
+$target.BeforeTargets = "AfterBuild"
 $task = $target.AddTask("Move")
 $task.SetParameter("SourceFiles", "`$(OutputPath)log4net.config")
 $task.SetParameter("DestinationFiles", "`$(OutputPath)`$(AssemblyName).exe.log4net")
-$project.Save() #persists the changes
+$project.Save()
 
 
 
