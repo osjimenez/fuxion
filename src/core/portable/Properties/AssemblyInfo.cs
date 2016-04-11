7f0dc10c93a9e4c1868e6027d000d02fb583d57e
Oscar Jimenez <social@ittxa.com>
2016-04-11 14:41:15 +0200

    Singleton Get by key BUG sovled

--- src/core/portable/Properties/AssemblyInfo.cs
+++ src/core/portable/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.3.1")]
-[assembly: AssemblyFileVersion("0.0.3.1")]
-[assembly: AssemblyInformationalVersion("0.0.3-alpha001")]
+[assembly: AssemblyVersion("0.0.3.2")]
+[assembly: AssemblyFileVersion("0.0.3.2")]
+[assembly: AssemblyInformationalVersion("0.0.3-alpha002")]
---
+++ src/core/test/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Fuxion.Test
+{
+    public class SingletonTest
+    {
+        [Fact]
+        public void Singleton_First()
+        {
+            var id = Guid.NewGuid();
+            Singleton.Add("oka", id);
+            var res = Singleton.Get<string>(id);
+            Assert.Same("oka", res);
+        }
+    }
+}
