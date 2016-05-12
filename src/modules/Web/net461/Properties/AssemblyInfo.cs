c7b8ba1f1963f49232a33629bf1b7c2405a24b05
Oscar Jimenez <social@ittxa.com>
2016-05-12 22:43:19 +0200

    FactoryControllerActivator in Fuxion.Web package

--- src/modules/Web/net461/Properties/AssemblyInfo.cs
+++ src/modules/Web/net461/Properties/AssemblyInfo.cs
@@ -1,4 +1,3 @@
-using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -10,10 +9,15 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Fuxion.Web")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b407f038-98e7-40bd-9f46-dc52a854cb9d")]
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -24,6 +28,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.3")]
-[assembly: AssemblyFileVersion("0.0.1.3")]
-[assembly: AssemblyInformationalVersion("0.0.1-alpha003")]
+[assembly: AssemblyVersion("0.0.1.10")]
+[assembly: AssemblyFileVersion("0.0.1.10")]
+[assembly: AssemblyInformationalVersion("0.0.1-alpha010")]
