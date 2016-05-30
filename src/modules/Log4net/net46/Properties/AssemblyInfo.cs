9acf5d258dd98f122868b5cd88db65843a129378
Oscar Jimenez <social@ittxa.com>
2016-05-30 13:21:40 +0200

    BUG in some projects with new net461 paths references

--- src/modules/Log4net/net46/Properties/AssemblyInfo.cs
+++ src/modules/Log4net/net46/Properties/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.3.9")]
-[assembly: AssemblyFileVersion("0.0.3.9")]
-[assembly: AssemblyInformationalVersion("0.0.3-alpha009")]
+[assembly: AssemblyVersion("0.0.3.10")]
+[assembly: AssemblyFileVersion("0.0.3.10")]
+[assembly: AssemblyInformationalVersion("0.0.3-alpha010")]
