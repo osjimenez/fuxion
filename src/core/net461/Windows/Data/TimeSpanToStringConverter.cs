e0690a38f8742ecb96f0c04a53515f692b729de1
Oscar Jimenez <social@ittxa.com>
2016-05-31 15:36:47 +0200

    BUG in TimeSpanToStringConverter when TimeSpan? is null

--- src/core/net461/Windows/Data/TimeSpanToStringConverter.cs
+++ src/core/net461/Windows/Data/TimeSpanToStringConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fuxion.Resources;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -10,41 +11,62 @@ namespace Fuxion.Windows.Data
 {
     public enum TimeSpanToStringMode
     {
-        Full,
-        OneTenant,
-        TwoTenant,
-        ThreeTenant,
+        PerElements,
+        PerTotals,
     }
     public class TimeSpanToStringConverter : GenericConverter<TimeSpan?,string>
     {
         public TimeSpanToStringMode Mode { get; set; }
+        public int NumberOfElements { get; set; } = 5;
         public override string Convert(TimeSpan? source, object parameter, CultureInfo culture)
         {
+            if (source == null || !source.HasValue) return null;
             var ts = source.Value;
             string res = "";
             switch (Mode)
             {
-                case TimeSpanToStringMode.OneTenant:
-                    if (ts.Days > 0) return $"{ts.Days} days, ";
-                    if (ts.Hours > 0) return $"{ts.Hours} hours, ";
-                    if (ts.Minutes > 0) return $"{ts.Minutes} minutes, ";
-                    return $"{ts.Seconds} seconds, ";
-                case TimeSpanToStringMode.TwoTenant:
-                    if (ts.Days > 0) res += $"{ts.Days} days, ";
-                    if (ts.Hours > 0) res += $"{ts.Hours} hours, ";
-                    if (ts.Minutes > 0) res += $"{ts.Minutes} minutes, ";
+                case TimeSpanToStringMode.PerElements:
+                    int count = 0;
+                    if (count >= NumberOfElements) return res.Trim(',', ' ');
+                    if (ts.Days > 0)
+                    {
+                        res += $"{ts.Days} {Strings.days}, ";
+                        count++;
+                    }
+                    if (count >= NumberOfElements) return res.Trim(',', ' ');
+                    if (ts.Hours > 0)
+                    {
+                        res += $"{ts.Hours} {Strings.hours}, ";
+                        count++;
+                    }
+                    if (count >= NumberOfElements) return res.Trim(',', ' ');
+                    if (ts.Minutes > 0)
+                    {
+                        res += $"{ts.Minutes} {Strings.minutes}, ";
+                        count++;
+                    }
+                    if (count >= NumberOfElements) return res.Trim(',', ' ');
+                    if (ts.Seconds > 0)
+                    {
+                        res += $"{ts.Seconds} {Strings.seconds}, ";
+                        count++;
+                    }
+                    if (count >= NumberOfElements) return res.Trim(',', ' ');
+                    if (ts.Milliseconds > 0)
+                    {
+                        res += $"{ts.Milliseconds} {Strings.milliseconds}, ";
+                        count++;
+                    }
+                    if (count >= NumberOfElements) return res.Trim(',', ' ');
+                    if (ts.Ticks > 0)
+                    {
+                        res += $"{ts.Ticks} {Strings.ticks}, ";
+                        count++;
+                    }
+                    if (count >= NumberOfElements) return res.Trim(',', ' ');
                     return res.Trim(',', ' ');
-                case TimeSpanToStringMode.ThreeTenant:
-                    if (ts.Days > 0) res += $"{ts.Days} days, ";
-                    if (ts.Hours > 0) res += $"{ts.Hours} hours, ";
-                    if (ts.Minutes > 0) res += $"{ts.Minutes} minutes, ";
-                    return res.Trim(',', ' ');
-                case TimeSpanToStringMode.Full:
                 default:
-                    res += $"{ts.Days} days, ";
-                    res += $"{ts.Hours} hours, ";
-                    res += $"{ts.Minutes} minutes, ";
-                    return res.Trim(',', ' ');
+                    return ts.ToString();
             }
         }
     }
---
+++ src/core/test/TimeSpanToStringConverter.test.cs
@@ -0,0 +1,34 @@
+﻿using Fuxion.Windows.Data;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Fuxion.Test
+{
+    public class TimeSpanToStringConverterTest
+    {
+        public TimeSpanToStringConverterTest(ITestOutputHelper output)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("es-ES");
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("es-ES");
+            this.output = output;
+        }
+        ITestOutputHelper output;
+        [Fact]
+        public void TimeSpanToStringConverter_First()
+        {
+            var con = new TimeSpanToStringConverter();
+            con.Mode = TimeSpanToStringMode.PerElements;
+            con.NumberOfElements = 3;
+            output.WriteLine($"3.18:53:58.1234567 => {con.Convert(TimeSpan.Parse("3.18:53:58.1234567"), null, CultureInfo.CurrentCulture)}");
+            output.WriteLine($"0.18:53:58.1234567 => {con.Convert(TimeSpan.Parse("0.18:53:58.1234567"), null, CultureInfo.CurrentCulture)}");
+            output.WriteLine($"0.00:53:58.1234567 => {con.Convert(TimeSpan.Parse("0.00:53:58.1234567"), null, CultureInfo.CurrentCulture)}");
+        }
+    }
+}
