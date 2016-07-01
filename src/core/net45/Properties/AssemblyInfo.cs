a8cf5017f60e803f4d24db7566900679ae576bd8
Oscar Jimenez <oscar@WAF.local>
2016-07-01 10:32:46 +0200

    Fuxion core migrated to net45

--- src/core/net45/Properties/AssemblyInfo.cs
+++ src/core/net45/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.3.89")]
-[assembly: AssemblyFileVersion("0.0.3.89")]
-[assembly: AssemblyInformationalVersion("0.0.3-alpha089")]
+[assembly: AssemblyVersion("0.0.3.90")]
+[assembly: AssemblyFileVersion("0.0.3.90")]
+[assembly: AssemblyInformationalVersion("0.0.3-alpha090")]
