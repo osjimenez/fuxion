b6c16677447af9ea906d456c733ffddcbf716b16
Oscar Jimenez <social@ittxa.com>
2017-09-13 08:22:04 +0200

    Change Fuxion.Windows to net461

--- src/.build/NuGet/Fuxion.Windows/ProcessFiles.cmd
+++ src/.build/NuGet/Fuxion.Windows/ProcessFiles.cmd
@@ -1,5 +1,5 @@
-mkdir src\.build\NuGet\Fuxion.Windows\nuspec\lib\net47
-copy  src\.build\NuGet\Fuxion.Windows\out~\Fuxion.Windows.dll src\.build\NuGet\Fuxion.Windows\nuspec\lib\net47\Fuxion.Windows.dll
-copy  src\.build\NuGet\Fuxion.Windows\out~\Fuxion.Windows.pdb src\.build\NuGet\Fuxion.Windows\nuspec\lib\net47\Fuxion.Windows.pdb
-mkdir src\.build\NuGet\Fuxion.Windows\nuspec\lib\net47\es
-copy  src\.build\NuGet\Fuxion.Windows\out~\es\Fuxion.Windows.resources.dll src\.build\NuGet\Fuxion.Windows\nuspec\lib\net47\es\Fuxion.Windows.resources.dll
+mkdir src\.build\NuGet\Fuxion.Windows\nuspec\lib\net461
+copy  src\.build\NuGet\Fuxion.Windows\out~\Fuxion.Windows.dll src\.build\NuGet\Fuxion.Windows\nuspec\lib\net461\Fuxion.Windows.dll
+copy  src\.build\NuGet\Fuxion.Windows\out~\Fuxion.Windows.pdb src\.build\NuGet\Fuxion.Windows\nuspec\lib\net461\Fuxion.Windows.pdb
+mkdir src\.build\NuGet\Fuxion.Windows\nuspec\lib\net461\es
+copy  src\.build\NuGet\Fuxion.Windows\out~\es\Fuxion.Windows.resources.dll src\.build\NuGet\Fuxion.Windows\nuspec\lib\net461\es\Fuxion.Windows.resources.dll
