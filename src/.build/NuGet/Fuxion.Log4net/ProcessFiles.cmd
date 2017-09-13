285e083284e2610eb20ad4eb5d88f6cdf00dc192
Oscar Jimenez <social@ittxa.com>
2017-09-13 08:53:32 +0200

    Testing net461 instead of net47

--- src/.build/NuGet/Fuxion.Log4net/ProcessFiles.cmd
+++ src/.build/NuGet/Fuxion.Log4net/ProcessFiles.cmd
@@ -1,6 +1,13 @@
 mkdir src\.build\NuGet\Fuxion.Log4net\nuspec\lib\net461
 copy  src\.build\NuGet\Fuxion.Log4net\out~\net461\Fuxion.Log4net.dll src\.build\NuGet\Fuxion.Log4net\nuspec\lib\net461\Fuxion.Log4net.dll
 copy  src\.build\NuGet\Fuxion.Log4net\out~\net461\Fuxion.Log4net.pdb src\.build\NuGet\Fuxion.Log4net\nuspec\lib\net461\Fuxion.Log4net.pdb
+mkdir src\.build\NuGet\Fuxion.Log4net\nuspec\lib\net462
+copy  src\.build\NuGet\Fuxion.Log4net\out~\net461\Fuxion.Log4net.dll src\.build\NuGet\Fuxion.Log4net\nuspec\lib\net462\Fuxion.Log4net.dll
+copy  src\.build\NuGet\Fuxion.Log4net\out~\net461\Fuxion.Log4net.pdb src\.build\NuGet\Fuxion.Log4net\nuspec\lib\net462\Fuxion.Log4net.pdb
+mkdir src\.build\NuGet\Fuxion.Log4net\nuspec\lib\net47
+copy  src\.build\NuGet\Fuxion.Log4net\out~\net461\Fuxion.Log4net.dll src\.build\NuGet\Fuxion.Log4net\nuspec\lib\net47\Fuxion.Log4net.dll
+copy  src\.build\NuGet\Fuxion.Log4net\out~\net461\Fuxion.Log4net.pdb src\.build\NuGet\Fuxion.Log4net\nuspec\lib\net47\Fuxion.Log4net.pdb
+
 mkdir src\.build\NuGet\Fuxion.Log4net\nuspec\tools
 copy  src\.build\NuGet\Fuxion.Log4net\out~\net461\install.ps1 src\.build\NuGet\Fuxion.Log4net\nuspec\tools\install.ps1
 mkdir src\.build\NuGet\Fuxion.Log4net\nuspec\content
