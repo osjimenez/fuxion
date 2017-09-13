5dba5a6810b468fa2f423cfa5f47b5ba5c35612c
Oscar Jimenez <social@ittxa.com>
2017-09-13 08:29:46 +0200

    Testing net461 instead of net47

--- src/.build/NuGet/Fuxion.Log4net/ProcessFiles.cmd
+++ src/.build/NuGet/Fuxion.Log4net/ProcessFiles.cmd
@@ -1,10 +1,10 @@
-mkdir src\.build\NuGet\Fuxion.Log4net\nuspec\lib\net47
-copy  src\.build\NuGet\Fuxion.Log4net\out~\net47\Fuxion.Log4net.dll src\.build\NuGet\Fuxion.Log4net\nuspec\lib\net47\Fuxion.Log4net.dll
-copy  src\.build\NuGet\Fuxion.Log4net\out~\net47\Fuxion.Log4net.pdb src\.build\NuGet\Fuxion.Log4net\nuspec\lib\net47\Fuxion.Log4net.pdb
+mkdir src\.build\NuGet\Fuxion.Log4net\nuspec\lib\net461
+copy  src\.build\NuGet\Fuxion.Log4net\out~\net461\Fuxion.Log4net.dll src\.build\NuGet\Fuxion.Log4net\nuspec\lib\net461\Fuxion.Log4net.dll
+copy  src\.build\NuGet\Fuxion.Log4net\out~\net461\Fuxion.Log4net.pdb src\.build\NuGet\Fuxion.Log4net\nuspec\lib\net461\Fuxion.Log4net.pdb
 mkdir src\.build\NuGet\Fuxion.Log4net\nuspec\tools
-copy  src\.build\NuGet\Fuxion.Log4net\out~\net47\install.ps1 src\.build\NuGet\Fuxion.Log4net\nuspec\tools\install.ps1
+copy  src\.build\NuGet\Fuxion.Log4net\out~\net461\install.ps1 src\.build\NuGet\Fuxion.Log4net\nuspec\tools\install.ps1
 mkdir src\.build\NuGet\Fuxion.Log4net\nuspec\content
-copy  src\.build\NuGet\Fuxion.Log4net\out~\net47\log4net.config src\.build\NuGet\Fuxion.Log4net\nuspec\content\log4net.config
+copy  src\.build\NuGet\Fuxion.Log4net\out~\net461\log4net.config src\.build\NuGet\Fuxion.Log4net\nuspec\content\log4net.config
                               
 mkdir src\.build\NuGet\Fuxion.Log4net\nuspec\lib\netstandard2.0
 copy  src\.build\NuGet\Fuxion.Log4net\out~\netstandard2.0\Fuxion.Log4net.dll src\.build\NuGet\Fuxion.Log4net\nuspec\lib\netstandard2.0\Fuxion.Log4net.dll
