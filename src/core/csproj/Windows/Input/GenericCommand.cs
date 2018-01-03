7b91f9bfa65c70e9400fafb169c92c655d652063
Oscar Jimenez <social@ittxa.com>
2018-01-03 13:36:27 +0100

    Generic Command

---
+++ src/core/csproj/Windows/Input/GenericCommand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Fuxion.Windows.Input
+{
+    public class GenericCommand : ICommand
+    {
+        public GenericCommand(Action action, Func<bool> canExecute = null)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+        Action action;
+        Func<bool> canExecute;
+        public event EventHandler CanExecuteChanged;
+        public bool CanExecute(object parameter)
+        {
+            return canExecute != null ? canExecute() : true;
+        }
+        public void Execute(object parameter)
+        {
+            action();
+        }
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+    public class GenericCommand<TParameter> : ICommand
+    {
+        public GenericCommand(Action<TParameter> action, Func<TParameter, bool> canExecute = null)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+        Action<TParameter> action;
+        Func<TParameter, bool> canExecute;
+        public event EventHandler CanExecuteChanged;
+        public bool CanExecute(object parameter)
+        {
+            if (parameter is TParameter par)
+                return canExecute != null ? canExecute(par) : true;
+            throw new InvalidCastException($"The parameter of type '{parameter.GetType().Name}' couldn't casted to '{typeof(TParameter).Name}' as was declared for command parameter.");
+        }
+        public void Execute(object parameter)
+        {
+            if (parameter is TParameter par)
+                action(par);
+            throw new InvalidCastException($"The parameter of type '{parameter.GetType().Name}' couldn't casted to '{typeof(TParameter).Name}' as was declared for command parameter.");
+        }
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
