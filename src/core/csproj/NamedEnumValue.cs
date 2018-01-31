d07d61eb267060600908393a61e7531584e9ec5d
Oscar Jimenez <social@ittxa.com>
2018-01-31 13:23:49 +0100

    NamedEnumValue and converters added

---
+++ src/core/csproj/NamedEnumValue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+namespace Fuxion
+{
+	public struct NamedEnumValue : IEquatable<Enum>
+	{
+		public NamedEnumValue(Enum value)
+		{
+			Value = value;
+			Name = value.GetType()
+				.GetField(value.ToString())
+				.GetCustomAttributes(false)
+				.OfType<DisplayAttribute>()
+				.FirstOrDefault()?.GetName() ?? value.ToString();
+		}
+		public string Name { get; }
+		public Enum Value { get; }
+		public override string ToString() => Name;
+		public static implicit operator NamedEnumValue(Enum @enum) => new NamedEnumValue(@enum);
+		public static implicit operator Enum(NamedEnumValue value) => value.Value;
+		public static bool operator ==(Enum @enum, NamedEnumValue value) => @enum.Equals(value.Value);
+		public static bool operator !=(Enum @enum, NamedEnumValue value) => !@enum.Equals(value.Value);
+		public static bool operator ==(NamedEnumValue value, Enum @enum) => @enum.Equals(value.Value);
+		public static bool operator !=(NamedEnumValue value, Enum @enum) => !@enum.Equals(value.Value);
+		public override bool Equals(object obj)
+		{
+			if (obj is Enum @enum)
+				return @enum.Equals(Value);
+			if (obj is NamedEnumValue value)
+				return value.Value.Equals(Value);
+			return false;
+		}
+		public override int GetHashCode() => Value.GetHashCode();
+		public bool Equals(Enum @enum) => @enum.Equals(Value);
+	}
+}
---
+++ src/modules/Windows/csproj/Data/EnumTypeToNamedEnumValueListConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuxion.Windows.Data
+{
+	public class EnumTypeToNamedEnumValueListConverter : GenericConverter<object, List<NamedEnumValue>, Type>
+	{
+		public override List<NamedEnumValue> Convert(object _, Type enumType, CultureInfo culture)
+			=> Enum.GetValues(enumType).Cast<Enum>().Select(e => new NamedEnumValue(e)).ToList();
+	}
+}
