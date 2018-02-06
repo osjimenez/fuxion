40c3bc7a516aab44ba32ea90f39b72530a1dba41
Oscar Jimenez <social@ittxa.com>
2018-02-06 11:06:49 +0100

    GenericCommand Execute and CanExecute methods casted and removed parameter as needed

--- src/core/csproj/Windows/Input/GenericCommand.cs
+++ src/core/csproj/Windows/Input/GenericCommand.cs
@@ -5,57 +5,57 @@ using System.Windows.Input;
 
 namespace Fuxion.Windows.Input
 {
-    public class GenericCommand : ICommand
-    {
-        public GenericCommand(Action action, Func<bool> canExecute = null)
-        {
-            this.action = action;
-            this.canExecute = canExecute;
-        }
-        Action action;
-        Func<bool> canExecute;
-        public event EventHandler CanExecuteChanged;
-        public bool CanExecute(object parameter)
-        {
-            return canExecute != null ? canExecute() : true;
-        }
-        public void Execute(object parameter)
-        {
-            action();
-        }
-        public void RaiseCanExecuteChanged()
-        {
-            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
-        }
-    }
-    public class GenericCommand<TParameter> : ICommand
-    {
-        public GenericCommand(Action<TParameter> action, Func<TParameter, bool> canExecute = null)
-        {
-            this.action = action;
-            this.canExecute = canExecute;
-        }
-        Action<TParameter> action;
-        Func<TParameter, bool> canExecute;
-        public event EventHandler CanExecuteChanged;
-        public bool CanExecute(object parameter)
-        {
-            if (parameter is TParameter par)
-                return canExecute != null ? canExecute(par) : true;
-            if (typeof(TParameter).IsNullable() && parameter == null)
-                return canExecute != null ? canExecute(default(TParameter)) : true;
-            throw new InvalidCastException($"The parameter of type '{parameter.GetType().Name}' couldn't casted to '{typeof(TParameter).Name}' as was declared for command parameter.");
-        }
-        public void Execute(object parameter)
-        {
-            if (parameter is TParameter par)
-                action(par);
-            else
-                throw new InvalidCastException($"The parameter of type '{parameter.GetType().Name}' couldn't casted to '{typeof(TParameter).Name}' as was declared for command parameter.");
-        }
-        public void RaiseCanExecuteChanged()
-        {
-            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
-        }
-    }
+	public class GenericCommand : ICommand
+	{
+		public GenericCommand(Action action, Func<bool> canExecute = null)
+		{
+			this.action = action;
+			this.canExecute = canExecute;
+		}
+
+		Action action;
+		Func<bool> canExecute;
+
+		public event EventHandler CanExecuteChanged;
+		bool ICommand.CanExecute(object parameter) => CanExecute();
+		public bool CanExecute() => canExecute != null ? canExecute() : true;
+		public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+		void ICommand.Execute(object parameter) => Execute();
+		public void Execute() => action();
+		
+	}
+	public class GenericCommand<TParameter> : ICommand
+	{
+		public GenericCommand(Action<TParameter> action, Func<TParameter, bool> canExecute = null)
+		{
+			this.action = action;
+			this.canExecute = canExecute;
+		}
+
+		Action<TParameter> action;
+		Func<TParameter, bool> canExecute;
+
+		public event EventHandler CanExecuteChanged;
+		bool ICommand.CanExecute(object parameter)
+		{
+			if (parameter is TParameter par)
+				return canExecute != null ? CanExecute(par) : true;
+			if (typeof(TParameter).IsNullable() && parameter == null)
+				return canExecute != null ? CanExecute(default(TParameter)) : true;
+			throw new InvalidCastException($"The parameter of type '{parameter.GetType().Name}' couldn't casted to '{typeof(TParameter).Name}' as was declared for command parameter.");
+		}
+		public bool CanExecute(TParameter parameter) => canExecute(parameter);
+		public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+		void ICommand.Execute(object parameter)
+		{
+			if (parameter is TParameter par)
+				Execute(par);
+			else
+				throw new InvalidCastException($"The parameter of type '{parameter.GetType().Name}' couldn't casted to '{typeof(TParameter).Name}' as was declared for command parameter.");
+		}
+		public void Execute(TParameter parameter) => action(parameter);
+		
+	}
 }
