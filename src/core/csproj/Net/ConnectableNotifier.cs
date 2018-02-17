74eb52ddaba692eab715fcac1441ce2741a69392
Oscar Jimenez <social@ittxa.com>
2018-02-17 21:41:12 +0100

    no message

--- src/core/csproj/Net/ConnectableNotifier.cs
+++ src/core/csproj/Net/ConnectableNotifier.cs
@@ -247,7 +247,7 @@ namespace Fuxion.Net
 					{
 						log.Error("Error '" + ex.GetType().Name + "' en el método 'OnKeepAlive': " + ex.Message, ex);
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-						Disconnect(true);
+						ReconnectOnFailure();
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 						break;
 					}
