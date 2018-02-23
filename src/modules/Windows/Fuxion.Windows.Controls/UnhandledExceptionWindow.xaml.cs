173c42103811bbe1d463fae6420caf79cc3b54ec
Oscar Jimenez <social@ittxa.com>
2018-02-23 14:48:35 +0100

    .

--- src/modules/Windows/Fuxion.Windows.Controls/UnhandledExceptionWindow.xaml.cs
+++ src/modules/Windows/Fuxion.Windows.Controls/UnhandledExceptionWindow.xaml.cs
@@ -1,51 +1,53 @@
-﻿using Fuxion.ComponentModel;
-using Fuxion.Windows.Documents;
+﻿using Fuxion.Windows.Documents;
 using Fuxion.Windows.Input;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Interop;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
 namespace Fuxion.Windows.Controls
 {
 	public partial class UnhandledExceptionWindow : Window, INotifyPropertyChanged
 	{
-		public UnhandledExceptionWindow(Exception ex)
+		public UnhandledExceptionWindow(Exception ex, Func<Task> sendReportFunc = null)
 		{
 			this.ex = ex;
+			this.sendReportFunc = sendReportFunc;
 			// Create commands
-			CloseCommand = new GenericCommand(() => this.Close(), () => Buttons == UnhandledExceptionWindowButtons.CloseWindow);
+			CloseCommand = new GenericCommand(() => Close(), () => Buttons.HasFlag(UnhandledExceptionWindowButtons.CloseWindow));
 			RestartApplicationCommand = new GenericCommand(() =>
 			{
-				System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+				Process.Start(Application.ResourceAssembly.Location);
 				Application.Current.Shutdown();
-			}, () => Buttons == UnhandledExceptionWindowButtons.RestartApplication);
+			}, () => Buttons.HasFlag(UnhandledExceptionWindowButtons.RestartApplication));
 			ShowDetailsCommand = new GenericCommand(() => ShowDetails = !ShowDetails, () => CanShowDetails);
+			SendReportCommand = new GenericCommand(async () =>
+			{
+				try
+				{
+					SendingReport = true;
+					await sendReportFunc.Invoke();
+				}
+				catch (Exception ex2)
+				{
+					Debug.WriteLine("");
+				}
+				finally
+				{
+					sendReportFunc = null;
+					SendReportCommand.RaiseCanExecuteChanged();
+					SendingReport = false;
+				}
+			}, () => sendReportFunc != null);
 			// Inicializar interfaz
 			InitializeComponent();
 			// Populate Document
 			Document.Blocks.AddRange(ex.ToBlocks());
 		}
 		public event PropertyChangedEventHandler PropertyChanged;
+		Func<Task> sendReportFunc;
 		Exception ex;
 		string _Message;
 		public string Message
@@ -57,7 +59,16 @@ namespace Fuxion.Windows.Controls
 				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Message)));
 			}
 		}
-
+		bool _SendingReport;
+		public bool SendingReport
+		{
+			get => _SendingReport;
+			set
+			{
+				_SendingReport = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SendingReport)));
+			}
+		}
 		UnhandledExceptionWindowButtons _Commands;
 		public UnhandledExceptionWindowButtons Buttons
 		{
@@ -91,61 +102,8 @@ namespace Fuxion.Windows.Controls
 				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ShowDetails)));
 			}
 		}
-		public static void WriteExceptionDetails(Exception exception, StringBuilder builderToFill, int level)
-		{
-			var indent = new string(' ', level);
-
-			if (level > 0)
-			{
-				builderToFill.AppendLine(indent + "=== INNER EXCEPTION ===");
-			}
-
-			Action<string> append = (prop) =>
-			{
-				var propInfo = exception.GetType().GetProperty(prop);
-				var val = propInfo.GetValue(exception);
-
-				if (val != null)
-				{
-					builderToFill.AppendFormat("{0}{1}: {2}{3}", indent, prop, val.ToString(), Environment.NewLine);
-				}
-			};
-
-			append("Message");
-			append("HResult");
-			append("HelpLink");
-			append("Source");
-			append("StackTrace");
-			append("TargetSite");
-
-			foreach (DictionaryEntry de in exception.Data)
-			{
-				builderToFill.AppendFormat("{0} {1} = {2}{3}", indent, de.Key, de.Value, Environment.NewLine);
-			}
-
-			if (exception.InnerException != null)
-			{
-				WriteExceptionDetails(exception.InnerException, builderToFill, ++level);
-			}
-		}
-		public string ReportContent
-		{
-			get
-			{
-				return ex.ToJson(settings: new JsonSerializerSettings().Transform<JsonSerializerSettings>(s => s.ContractResolver = new CustomResolver()));
-			}
-		}
-		GenericCommand<string> _SendReportCommand;
-		public GenericCommand<string> SendReportCommand
-		{
-			get => _SendReportCommand;
-			set
-			{
-				_SendReportCommand = value;
-				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SendReportCommand)));
-			}
-		}
-		public GenericCommand ShowDetailsCommand { get; set; }
+		public GenericCommand SendReportCommand { get; private set; }
+		public GenericCommand ShowDetailsCommand { get; private set; }
 		public GenericCommand CloseCommand { get; private set; }
 		public GenericCommand RestartApplicationCommand { get; private set; }
 
@@ -156,35 +114,11 @@ namespace Fuxion.Windows.Controls
 			FontFamily = new FontFamily("Segoe UI")
 		};
 	}
+	[Flags]
 	public enum UnhandledExceptionWindowButtons
 	{
-		CloseWindow,
-		RestartApplication
-	}
-	class CustomResolver : DefaultContractResolver
-	{
-		protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
-		{
-			JsonProperty property = base.CreateProperty(member, memberSerialization);
-
-			property.ShouldSerialize = instance =>
-			{
-				try
-				{
-					PropertyInfo prop = (PropertyInfo)member;
-					if (prop.CanRead)
-					{
-						prop.GetValue(instance, null);
-						return true;
-					}
-				}
-				catch
-				{
-				}
-				return false;
-			};
-
-			return property;
-		}
+		CloseWindow = 1,
+		RestartApplication = 2,
+		CloseWindowAndRestartApplication = 3
 	}
 }
