891f74adb1e5fe9e7e79b628e985b7297ec79e11
Oscar Jimenez <social@ittxa.com>
2018-03-14 16:12:02 +0100

    Consolidate NuGet

--- src/core/csproj/Web/Patchable.cs
+++ src/core/csproj/Web/Patchable.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CSharp.RuntimeBinder;
+﻿//using Microsoft.CSharp.RuntimeBinder;
+using Microsoft.CSharp.RuntimeBinder;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections;
