2011027e000b982eb32ba3b942932b3963942d37
Oscar Jimenez <social@ittxa.com>
2018-03-23 10:26:35 +0100

    EnumTypeToNamedEnumValueListConverter can be ordered alphabetically

--- src/modules/Windows/csproj/Data/EnumTypeToNamedEnumValueListConverter.cs
+++ src/modules/Windows/csproj/Data/EnumTypeToNamedEnumValueListConverter.cs
@@ -9,7 +9,12 @@ namespace Fuxion.Windows.Data
 {
 	public class EnumTypeToNamedEnumValueListConverter : GenericConverter<object, List<NamedEnumValue>, Type>
 	{
+		public bool IsAlphabeticallyOrdered { get; set; }
 		public override List<NamedEnumValue> Convert(object _, Type enumType, CultureInfo culture)
-			=> Enum.GetValues(enumType).Cast<Enum>().Select(e => new NamedEnumValue(e)).ToList();
+		{
+			var res = Enum.GetValues(enumType).Cast<Enum>().Select(e => new NamedEnumValue(e)).ToList();
+			if (IsAlphabeticallyOrdered) res = res.OrderBy(e => e.Name).ToList();
+			return res;
+		}
 	}
 }
