1a6b43931ff7176e1181a7a70a676d20f258af41
Oscar Jimenez <social@ittxa.com>
2018-04-19 13:07:43 +0200

    .

--- src/core/csproj/Net/ConnectableNotifier.cs
+++ src/core/csproj/Net/ConnectableNotifier.cs
@@ -162,76 +162,23 @@ namespace Fuxion.Net
 					connectionTask.OnCancelRequested(() => SetValue(IsConnectCancellationRequested, true, nameof(IsConnectCancellationRequested)));
 					connectionTask.Start();
 					return connectionTask;
-					break;
 				case ConnectionState.Opening:
 					firstTryResultFunc = new Func<Task<bool>>(() => Task.FromResult(false));
 					return connectionTask;
-					break;
 				case ConnectionState.Closing:
 					firstTryResultFunc = new Func<Task<bool>>(() => Task.FromResult(false));
 					return disconnectionTask.ContinueWith((task) => Connect());
-					break;
 				default:
 					throw new NotImplementedException($"El estado '{State}' no ha sido implementado en la operación de conexión.");
 			}
 		}
-		public Task Connect()
-		{
-			return Connect(out var _);
-			//switch (State)
-			//{
-			//	case ConnectionState.Opened: //Si esta conectado lanzo una excepción
-			//		throw new InvalidOperationException("No se puede conectar porque la conexión ya esta activa.");
-			//	case ConnectionState.Created: //Si esta creado o en error creo una nueva tarea de conexión
-			//	case ConnectionState.Faulted:
-			//	case ConnectionState.Closed:
-			//		State = ConnectionState.Opening;
-			//		connectionTask = TaskManager.Create(async () =>
-			//		{
-			//			SetValue(IsConnectCancellationRequested, true, nameof(IsConnectCancellationRequested));
-			//			while (!IsConnectCancellationRequested)
-			//			{
-			//				try
-			//				{
-			//					await OnConnect();
-			//					LastConnectionAttemptErrorMessage = null;
-			//					StartKeepAlive();
-			//					State = ConnectionState.Opened;
-			//					break;
-			//				}
-			//				catch (Exception ex)
-			//				{
-			//					log.Error($"Error '{ex.GetType().Name}' en el método '{nameof(OnConnect)}' de la clase '{GetType().GetSignature(false)}' (*)\r\n{ex.Message}", ex);
-			//					LastConnectionAttemptErrorMessage = ex.Message;
-			//					if (ConnectionMode == ConnectionMode.Manual)
-			//					{
-			//						State = ConnectionState.Faulted;
-			//						break;
-			//					}
-			//					connectionTask.Sleep(AutomaticConnectionModeRetryInterval);
-			//				}
-			//			}
-			//		});
-			//		connectionTask.OnCancelRequested(() => SetValue(IsConnectCancellationRequested, true, nameof(IsConnectCancellationRequested)));
-			//		connectionTask.Start();
-			//		await connectionTask;
-			//		break;
-			//	case ConnectionState.Opening:
-			//		await connectionTask;
-			//		break;
-			//	case ConnectionState.Closing:
-			//		await disconnectionTask.ContinueWith((task) => Connect());
-			//		break;
-			//	default:
-			//		throw new NotImplementedException($"El estado '{State}' no ha sido implementado en la operación de conexión.");
-			//}
-		}
+		public Task Connect() => Connect(out var _);
 		#endregion
 		#region Disconnect
 		protected bool IsDisconnectCancellationRequested => disconnectionTask.IsCancellationRequested();
 		Task disconnectionTask;
 		protected abstract Task OnDisconnect();
-		async Task Disconnect(bool mustClose)
+		Task Disconnect(bool mustClose)
 		{
 			switch (State)
 			{
@@ -239,8 +186,7 @@ namespace Fuxion.Net
 				case ConnectionState.Closed:
 					throw new InvalidOperationException("No se puede desconectar porque la conexión no esta activa.");
 				case ConnectionState.Closing:
-					await disconnectionTask;
-					break;
+					return disconnectionTask;
 				case ConnectionState.Faulted:
 				case ConnectionState.Opening:
 				case ConnectionState.Opened:
@@ -266,8 +212,7 @@ namespace Fuxion.Net
 					});
 					disconnectionTask.OnCancelRequested(() => SetValue(IsDisconnectCancellationRequested, true, nameof(IsDisconnectCancellationRequested)));
 					disconnectionTask.Start();
-					await disconnectionTask;
-					break;
+					return disconnectionTask;
 				default:
 					throw new NotImplementedException($"El estado '{State}' no ha sido implementado en la operación de desconexión.");
 			}
@@ -279,10 +224,10 @@ namespace Fuxion.Net
 			if (ConnectionMode == ConnectionMode.Automatic)
 			{
 				await Disconnect(false);
-				//var f = new Func<Task<bool>>(() => Task.FromResult(true));
-				Connect(out var f, timeout);
-				return await f();
-				//return true;
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+				Connect(out var firstTryResultFunc, timeout);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+				return await firstTryResultFunc();
 			}
 			return false;
 		}
