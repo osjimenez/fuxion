3d32214624d8cfbcac41bdfdd7aa9863c0ae1d21
Oscar Jimenez <social@ittxa.com>
2018-11-09 11:55:19 +0100

    Update Nugets

--- src/modules/Windows/csproj/Data/BooleanToVisibilityConverter.cs
+++ src/modules/Windows/csproj/Data/BooleanToVisibilityConverter.cs
@@ -1,28 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Data;
 
 namespace Fuxion.Windows.Data
 {
-    public class BooleanToVisibilityConverter : GenericConverter<bool,Visibility>
-    {
-        public Visibility TrueValue { get; set; } = Visibility.Visible;
-        public Visibility FalseValue { get; set; } = Visibility.Collapsed;
+	public class BooleanToVisibilityConverter : GenericConverter<bool, Visibility>
+	{
+		public Visibility TrueValue { get; set; } = Visibility.Visible;
+		public Visibility FalseValue { get; set; } = Visibility.Collapsed;
 
-        public override Visibility Convert(bool source, CultureInfo culture)
-        {
-            return source ? TrueValue : FalseValue;
-        }
-        public override bool ConvertBack(Visibility result, CultureInfo culture)
-        {
-            if (result == TrueValue) return true;
-            if (result == FalseValue) return false;
-            throw new NotSupportedException($"The value '{result}' is not supported for '{nameof(ConvertBack)}' method");
-        }
-    }
+		public override Visibility Convert(bool source, CultureInfo culture)
+		{
+			return source ? TrueValue : FalseValue;
+		}
+		public override bool ConvertBack(Visibility result, CultureInfo culture)
+		{
+			if (result == TrueValue)
+			{
+				return true;
+			}
+
+			if (result == FalseValue)
+			{
+				return false;
+			}
+
+			throw new NotSupportedException($"The value '{result}' is not supported for '{nameof(ConvertBack)}' method");
+		}
+	}
 }
