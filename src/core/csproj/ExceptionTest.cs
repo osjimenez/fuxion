0d9d62d311d319afa56de2308eea9cb4d6144bd3
Oscar Jimenez <social@ittxa.com>
2018-11-21 13:41:19 +0100

    Test SourceLink

---
+++ src/core/csproj/ExceptionTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fuxion
+{
+	public class ExceptionTest
+	{
+		public void LaunchException()
+		{
+			throw new Exception("Exception for testing purpose");
+		}
+	}
+}
