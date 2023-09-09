3dde227ed484fd14ac27adc09c6d7da1c0067e1d
Oscar Jimenez <oscar@lyberum.com>
2023-09-09 16:59:52 +0200

    Process TimeRecords

--- src/core/Fuxion/Collections/Generic/GenericComparer.cs
+++ src/core/Fuxion/Collections/Generic/GenericComparer.cs
@@ -1,8 +1,6 @@
 ﻿namespace Fuxion.Collections.Generic;
 
-public class GenericComparer<T> : Comparer<T>
+public class GenericComparer<T>(Func<T?, T?, int> comparisonFunction) : Comparer<T>
 {
-	public GenericComparer(Func<T?, T?, int> comparisonFunction) => this.comparisonFunction = comparisonFunction;
-	readonly Func<T?, T?, int> comparisonFunction;
 	public override int Compare(T? x, T? y) => comparisonFunction(x, y);
 }
