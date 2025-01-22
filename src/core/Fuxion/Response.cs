f63140f60d4522be2b04eb813cc370898b151add
Oscar Jimenez <oscar@lyberum.com>
2025-01-22 19:34:41 +0100

    .

--- src/core/Fuxion/Response.cs
+++ src/core/Fuxion/Response.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Text.Json.Serialization;
 
 namespace Fuxion;
-
 
 public interface IResponse
 {
@@ -23,12 +19,13 @@ public interface IResponse
 	internal object? GetPayloadObject() => null;
 #endif
 }
+
 public interface IResponse<out TPayload> : IResponse
 {
+	TPayload Payload { get; }
 #if !NETSTANDARD2_0 && !NET462
 	object? IResponse.GetPayloadObject() => Payload;
 #endif
-	TPayload Payload { get; }
 }
 
 public abstract class Response(bool isSuccess, string? message) : IResponse
@@ -39,42 +36,47 @@ public abstract class Response(bool isSuccess, string? message) : IResponse
 #endif
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public string? Message { get; } = message;
-
 #if NETSTANDARD2_0 || NET462
 	object? IResponse.GetPayloadObject() => null;
 #endif
-
 	public static IResponseFactory Get { get; } = null!;
 }
 
 public interface IResponseFactory;
 
 [JsonConverter(typeof(JsonStringEnumConverter))]
-public enum ErrorType { NotFound, Invalid, Unauthorized, Forbidden, Conflict, InternalError }
+public enum ErrorType
+{
+	NotFound,
+	Invalid,
+	Unauthorized,
+	Forbidden,
+	Conflict,
+	InternalError
+}
+
 public class SuccessResponse(string? message = null) : Response(true, message);
+
 public class SuccessResponse<TPayload>(TPayload value, string? message = null) : SuccessResponse(message), IResponse<TPayload>
 {
 	public TPayload Payload { get; } = value;
-
 	public static implicit operator TPayload(SuccessResponse<TPayload> res) => res.Payload;
 }
-public class ErrorResponse(string message, int? code = null, object? type = null, Exception? exception = null)
-	: Response(false, message)
+
+public class ErrorResponse(string message, int? code = null, object? type = null, Exception? exception = null) : Response(false, message)
 {
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public int? ErrorCode { get; } = code;
-
 	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 	public object? ErrorType { get; } = type;
-
 	[JsonIgnore]
 	public Exception? Exception { get; } = exception;
 }
+
 public class ErrorResponse<TPayload>(string message, TPayload value, int? code = null, object? type = null, Exception? exception = null)
 	: ErrorResponse(message, code, type, exception), IResponse<TPayload>
 {
 	public TPayload Payload { get; } = value;
-
 	public static implicit operator TPayload(ErrorResponse<TPayload> res) => res.Payload;
 }
 
@@ -85,33 +87,24 @@ public static class ResponseExtensions
 		foreach (var res in responses)
 			if (res.IsError)
 				return res;
-		if (responses.Any(r => r.IsError))
-			return new ErrorResponse<IEnumerable<IResponse>>("Multiple errors", responses.Where(r => r.IsError));
+		if (responses.Any(r => r.IsError)) return new ErrorResponse<IEnumerable<IResponse>>("Multiple errors", responses.Where(r => r.IsError));
 		return new SuccessResponse<IEnumerable<IResponse>>(responses);
 	}
-
 	public static SuccessResponse Success(this IResponseFactory me) => new();
 	public static SuccessResponse SuccessMessage(this IResponseFactory me, string? message = null) => new(message);
 	public static SuccessResponse<TPayload> Success<TPayload>(this IResponseFactory me, TPayload payload, string? message = null) => new(payload, message);
-
 	public static ErrorResponse Error(this IResponseFactory me, string message) => new(message);
 	public static ErrorResponse<TPayload> Error<TPayload>(this IResponseFactory me, string message, TPayload payload) => new(message, payload);
-
-	public static bool IsErrorType(this IResponse res, object type) =>
-		res is ErrorResponse er && er.ErrorType?.Equals(type) == true;
+	public static bool IsErrorType(this IResponse res, object type) => res is ErrorResponse er && er.ErrorType?.Equals(type) == true;
 	// Specific error types
-
 	public static ErrorResponse NotFound(this IResponseFactory me, string message = "Not found") => new(message, type: ErrorType.NotFound);
 	public static ErrorResponse<TPayload> NotFound<TPayload>(this IResponseFactory me, TPayload payload, string message = "Not found") => new(message, payload, type: ErrorType.NotFound);
 	public static bool IsNotFound(this IResponse res) => res.IsErrorType(ErrorType.NotFound);
-
 	public static ErrorResponse Invalid(this IResponseFactory me, string message = "Invalid") => new(message, type: ErrorType.Invalid);
-
 	public static bool IsInvalid(this IResponse res) => res is ErrorResponse { ErrorType: ErrorType.Invalid };
 	public static bool IsUnauthorized(this IResponse res) => res is ErrorResponse { ErrorType: ErrorType.Unauthorized };
 	public static bool IsForbidden(this IResponse res) => res is ErrorResponse { ErrorType: ErrorType.Forbidden };
 	public static bool IsConflict(this IResponse res) => res is ErrorResponse { ErrorType: ErrorType.Conflict };
 	public static bool IsInternalError(this IResponse res) => res is ErrorResponse { ErrorType: ErrorType.InternalError };
-
 	public static object? GetPayload(this IResponse res) => res.GetPayloadObject();
 }
